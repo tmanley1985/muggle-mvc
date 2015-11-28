5476b63b7ad5391bdfa841aa0a80718e48732b89
tmanley1985 <tmanley1985@gmail.com>
2015-11-28 13:22:27 -0500

    Parsing url, sanitizing it and getting params

--- app/core/App.php
+++ app/core/App.php
@@ -10,11 +10,19 @@ class App {
 
 	public function __construct()
 	{
-
+		$url = $this->parse_url();
 	}
 
 	protected function parse_url()
 	{
-		
+		if( $_GET['url']) {
+
+			$trimmed_url = rtrim( $_GET['url'], '/' );
+
+			$sanitized_url = filter_var( $trimmed_url, FILTER_SANITIZE_URL );
+
+			return $url_parameters = explode( '/', $sanitized_url );
+
+		}
 	}
 }	
