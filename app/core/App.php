049a09912754a98bcccefc84c08f9aac64df0298
tmanley1985 <tmanley1985@gmail.com>
2015-11-28 15:38:10 -0500

    Grabbing method and params from url and calling method on controller with params

--- app/core/App.php
+++ app/core/App.php
@@ -8,9 +8,35 @@ class App {
 
 	protected $params = [];
 
+	/**
+	 * [__construct Parses url ]
+	 *
+	 * @return   [<description>]
+	 */
+	
 	public function __construct()
 	{
 		$url = $this->parse_url();
+
+		if( file_exists( '../app/controllers/' . $url[0] . '.php')) {
+			$this->controller = $url[0];
+			unset($url[0]);
+		}
+
+		require_once '../app/controllers/' . $this->controller . '.php';
+
+		$this->controller = new $this->controller;
+
+		if( isset( $url[1] ) ) {
+
+			$this->method = $url[1];
+			unset($url[1]);
+
+		}
+		
+		$this->params = $url ? array_values( $url ) : [];
+
+		call_user_func_array([$this->controller, $this->method ], $this->params );
 	}
 
 	protected function parse_url()
