866fc3c88d219559129a0aa45200da680c5a7e9c
tmanley1985 <tmanley1985@gmail.com>
2015-11-29 22:07:13 -0500

    Renamed folder

---
+++ app/Core/App.php
@@ -0,0 +1,56 @@
+<?php namespace App\Core;
+
+class App {
+	
+	protected $controller = 'home';
+	
+	protected $method = 'index';
+
+	protected $params = [];
+
+	/**
+	 * [__construct Parses url ]
+	 *
+	 * @return   [<description>]
+	 */
+	
+	public function __construct()
+	{
+		$url = $this->parse_url();
+
+		if( file_exists( '../app/controllers/' . $url[0] . '.php')) {
+			$this->controller = $url[0];
+			unset($url[0]);
+		}
+
+		// require_once '../app/controllers/' . $this->controller . '.php';
+		$controller = 'App\Controllers\\' . $this->controller;
+		$this->controller = new $controller;
+
+		var_dump($this->controller);
+
+		if( isset( $url[1] ) ) {
+
+			$this->method = $url[1];
+			unset($url[1]);
+
+		}
+		
+		$this->params = $url ? array_values( $url ) : [];
+
+		call_user_func_array([$this->controller, $this->method ], $this->params );
+	}
+
+	protected function parse_url()
+	{
+		if( $_GET['url']) {
+
+			$trimmed_url = rtrim( $_GET['url'], '/' );
+
+			$sanitized_url = filter_var( $trimmed_url, FILTER_SANITIZE_URL );
+
+			return $url_parameters = explode( '/', $sanitized_url );
+
+		}
+	}
+}	
---
+++ app/Core/Controller.php
@@ -0,0 +1,9 @@
+<?php namespace App\Core;
+
+class Controller {
+	
+	public function view( $view, $data = [] )
+	{
+		require_once '../app/views/' . $view . '.php';
+	}
+}
