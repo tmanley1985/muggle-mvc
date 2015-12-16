7348e043b3aacaa659f994e1ce8361b76f9f2da4
tmanley1985 <tmanley1985@gmail.com>
2015-12-16 11:24:06 -0500

    Added docs

--- app/Core/Http/Request.php
+++ app/Core/Http/Request.php
@@ -3,7 +3,10 @@
 class Request
 {
 	protected $global_input;
-
+	/**
+	 * Creates Instance of the Request
+	 * @param array $global_input POST Superglobal
+	 */
 	public function __construct(array $global_input)
 	{
 
@@ -12,9 +15,15 @@ class Request
 		$this->global_input = $this->sanitize_input( $global_input );
 
 	}
-
+	/**
+	 * Sanitizes Request
+	 * @param  array  $request Raw Request
+	 * @return array $request Sanitized Request
+	 */
 	protected function sanitize_input( array $request )
 	{
+		// Maps filter function to each member in the request
+		
 		$request = array_map( function( $input ){
 
 			$input = trim($input);
@@ -25,7 +34,10 @@ class Request
 
 		return $request;
 	}
-
+	/**
+	 * Public Getter for Request
+	 * @return array $request
+	 */
 	public function getInput()
 	{
 		return $request_data = $this->global_input;
