f80f7a7e1a05296756242c11a70c6298e93beea7
tmanley1985 <tmanley1985@gmail.com>
2015-12-16 14:13:12 -0500

    Added docs

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -2,6 +2,12 @@
 
 class Controller {
 	
+	/**
+	 * Requires View file
+	 * @param  string $view Filename
+	 * @param  array  $data Optional input to be passed
+	 * @return void
+	 */
 	public function view( $view, $data = [] )
 	{
 		require_once '../app/views/' . $view . '.php';
