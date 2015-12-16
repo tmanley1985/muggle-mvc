9e9c0a14cfa5fbcc8d01b4f0b3423636d1b7e096
tmanley1985 <tmanley1985@gmail.com>
2015-12-16 14:12:29 -0500

    DB Singleton

---
+++ app/Core/Database/DB.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Core\Database;
+
+use App\Core\Config\Config;
+
+class DB
+{
+	
+
+    /**
+     * @var Singleton instance of DB
+     */
+    private static $db;
+    private $host;
+    private $db_name;
+    private $user;
+    private $password;
+    private $port;
+    
+    /**
+     * Returns the *Singleton* instance of this class.
+     * @return Singleton The *DB* instance.
+     */
+    public static function getInstance( )
+    {
+    	$host = DB::getHost();
+    	$db_name = DB::getDBName();
+    	$user = DB::getUser();
+    	$password = DB::getPassword();
+    	$port = DB::getPort();
+    	
+        if (null === static::$db) {
+            static::$db = new static( $host, $db_name, $user, $password, $port );
+        }
+        
+        return static::$db;
+    }
+
+    /**
+     * Protected constructor to prevent creating a new instance of the
+     * *Singleton* via the `new` operator from outside of this class.
+     */
+    protected function __construct( $host, $db_name, $user, $password, $port )
+    {
+    	$this->host    = $host;
+    	$this->db_name = $db_name;
+    	$this->user    = $user;
+    	$this->password    = $password;
+    	$this->port    = $port;
+
+    	try {
+
+    		return new PDO( "mysql:host={$host};dbname={$db_name};port={$port}", $user , $password );
+
+    	} catch (Exception $e) {
+    		
+    	}
+    }
+
+    /**
+     * Private clone method to prevent cloning of the instance of the
+     * *Singleton* instance.
+     *
+     * @return void
+     */
+    private function __clone()
+    {
+    }
+
+    /**
+     * Private unserialize method to prevent unserializing of the *Singleton*
+     * instance.
+     *
+     * @return void
+     */
+    private function __wakeup()
+    {
+    }
+}
